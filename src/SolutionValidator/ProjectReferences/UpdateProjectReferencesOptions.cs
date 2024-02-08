5270f710bcbc4edbdf3feb2516e08265c359bf0d
Ljubo Nikolić <ljubonikolic@gmail.com>
2024-02-08 09:40:44 +0000

    fix format errors

--- src/SolutionValidator/ProjectReferences/UpdateProjectReferencesOptions.cs
+++ src/SolutionValidator/ProjectReferences/UpdateProjectReferencesOptions.cs
@@ -1,10 +1,10 @@
-﻿using Bulldog;
-using CommandLine;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Bulldog;
+using CommandLine;
 
 namespace SolutionValidator.ProjectReferences
 {
